306faa0cbab98ad753aa33fec45eb4a482cfe2f1
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-01 23:27:47 +0600

    Node mongodb basic setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config()
+const app = express();
+app.use(cors());
+app.use(express());
+
+const port = process.env.PORT || 5000;
+
+app.get("/",(req, res) => {
+    res.send("Server is running")
+})
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n72f5gi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+})
